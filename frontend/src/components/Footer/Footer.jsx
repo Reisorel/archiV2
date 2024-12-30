079fe53dc92cea3095a1cd4a8c76c34e4e69370c
Francois Lerosier <francois.lerosier@gmail.com>
2024-12-30 16:45:27 +0100

    finished footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,6 +3,14 @@ import ordre from "../../assets/logos/ordre.jpg";
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import upChevron from "../../assets/icons/up-arrow.svg";
+import { CiMail } from "react-icons/ci";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { CiInstagram } from "react-icons/ci";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io";
+
+import insta from "../../assets/icons/insta.svg";
+
 
 // Enregistrement du plugin GSAP
 gsap.registerPlugin(ScrollToPlugin);
@@ -20,30 +28,58 @@ function Footer() {
   return (
     <footer id="footer">
       <div className="footer-container">
+        <div className="left-paragraph">
+          <p>
+            <strong>Architecte diplômée d'état</strong>
+          </p>
+          <p> Habilitée à la Maîtrise</p>
+          <p>d'Oeuvre en son Nom Propre</p>
+          <div className="ordre">
+            <img src={ordre} alt="ordre" />
+          </div>
+        </div>
 
-      <div className="ordre">
-        <img src={ordre} alt="" />
-      </div>
+        <div className="center-paragraph">
+          <p>
+            <strong>Agence d'architecture basée à Rennes</strong>
+          </p>
+          <p>BRETAGNE / NORMANDIE / PARIS</p>
+          <div className="chevron" onClick={handleScrollToTop}>
+            <img src={upChevron} alt="up-chevron" />
+          </div>
+        </div>
 
-      <div className="footer-paragraph">
-        <h3>A Propos</h3>
-        <p>Voici un exemple de paragraphe devant se situer dans le flow du footer-paragrah</p>
-      </div>
+        <div className="right-paragraph">
+          <p>
+            <strong>T: 06 88 59 75 02</strong>
+          </p>
+          <p>cassandre.architecte@gmail.com</p>
+          <div className="icon-container">
+            <CiInstagram
+              className="icon"
+              role="button" /* Indique que c'est cliquable */
+              tabIndex={0} /* Rendre l'icône navigable avec le clavier */
+              onClick={() =>
+                window.open(
+                  "https://www.instagram.com/cassandremrn_architecte?igsh=MWw5Z2pzOGI1NnYwaQ==",
+                  "_blank"
+                )
+              }
+            />
+            <IoLogoLinkedin
+              className="icon"
+              role="button" /* Indique que c'est cliquable */
+              tabIndex={0} /* Rendre l'icône navigable avec le clavier */
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/cassandre-marion-0ab776128/",
+                  "_blank"
+                )
+              }
+            />
+          </div>
 
-      <div className="footer-paragraph">
-        <h3>Contact</h3>
-      </div>
-
-      <div className="footer-paragraph">
-        <h3>Réseaux</h3>
-      </div>
-
-
-      {/* <p>2024 - CASSANDRE MARION ARCHITECTURE</p> */}
-
-      <div className="chevron" onClick={handleScrollToTop}>
-        <img src={upChevron} alt="up-chevron" />
-      </div>
+        </div>
       </div>
     </footer>
   );
