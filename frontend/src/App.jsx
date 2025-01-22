af5bb32219d6130a4f4e7e5f425b082227e150e1
Francois Lerosier <francois.lerosier@gmail.com>
2025-01-22 10:10:09 +0100

    modified router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Background from "./components/Background/Background";
 import CustomCursor from "./components/Cursor/Cursor";
-import Router from "./routes/Router"; // Import du router
-
+import Router from "./routes/Router"; 
 
 function App() {
   return (
