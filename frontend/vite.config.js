1e0ab6baa898f15e04551bec3b6276d181db3cbb
Francois Lerosier <francois.lerosier@gmail.com>
2025-02-05 19:04:57 +0100

    try11 modified vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5000, // Définit le port du serveur sur 5000
+  root: '.', // Définit la racine du projet
+  publicDir: 'public', // Dossier pour les assets publics
+  build: {
+    outDir: 'dist', // Assure-toi que le build sort bien dans "dist"
+    emptyOutDir: true, // Supprime le contenu du dossier avant de builder
+    rollupOptions: {
+      input: 'index.html' // Assure que Vite trouve bien l'index
+    }
   },
-})
+  server: {
+    port: 5000, // Définit le port du serveur de dev sur 5000
+    strictPort: true
+  }
+});
+
