172afbf2d1de14223dbb9066966b487dadca4f52
Francois Lerosier <francois.lerosier@gmail.com>
2025-03-03 14:50:54 +0100

    added vercel analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer2 from "./components/Footer2/Footer2";
 import Background from "./components/Background/Background";
 import CustomCursor from "./components/Cursor/Cursor";
 import Router from "./routes/Router";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
+
   return (
     <>
       <div className="App">
@@ -17,8 +19,9 @@ function App() {
           <Background>
             <Router />
           </Background>
-          <Footer2 key={location.pathname} />
-          </BrowserRouter>
+          <Footer2/>
+          <Analytics /> {/* Ajout de Vercel Analytics ici */}
+        </BrowserRouter>
       </div>
     </>
   );
